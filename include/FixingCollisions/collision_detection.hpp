53d338bb0ea656a837d180aa431d76a292661938
zfergus <zfergus@nyu.edu>
2019-02-04 13:55:02 -0500

    Added format options

--- include/FixingCollisions/collision_detection.hpp
+++ include/FixingCollisions/collision_detection.hpp
@@ -18,37 +18,29 @@ typedef std::shared_ptr<Impact> ImpactPtr;
 typedef std::vector<ImpactPtr> Impacts;
 typedef std::shared_ptr<Impacts> ImpactsPtr;
 
-enum DetectionMethod {
-    BRUTE_FORCE,
-    HASH_MAP
-};
+enum DetectionMethod { BRUTE_FORCE, HASH_MAP };
 
-ImpactPtr detect_edge_vertex_collision(
-    const Eigen::MatrixXd& vertex0_t0,
-    const Eigen::MatrixXd& vertex0_t1,
-    const Eigen::MatrixXd& edge_vertex1_t0,
+ImpactPtr detect_edge_vertex_collision(const Eigen::MatrixXd& vertex0_t0,
+    const Eigen::MatrixXd& vertex0_t1, const Eigen::MatrixXd& edge_vertex1_t0,
     const Eigen::MatrixXd& edge_vertex1_t1,
     const Eigen::MatrixXd& edge_vertex2_t0,
     const Eigen::MatrixXd& edge_vertex2_t1);
 
 // General detection algorithm
-ImpactsPtr detect_edge_vertex_collisions(
-    const Eigen::MatrixXd& vertices_t0,
+ImpactsPtr detect_edge_vertex_collisions(const Eigen::MatrixXd& vertices_t0,
     const Eigen::MatrixXd& vertices_t1,
     const Eigen::Matrix<int, Eigen::Dynamic, 2>& edges,
     DetectionMethod method = BRUTE_FORCE);
 
 // Brute-force detection algorithm
 ImpactsPtr detect_edge_vertex_collisions_brute_force(
-    const Eigen::MatrixXd& vertices_t0,
-    const Eigen::MatrixXd& vertices_t1,
+    const Eigen::MatrixXd& vertices_t0, const Eigen::MatrixXd& vertices_t1,
     const Eigen::Matrix<int, Eigen::Dynamic, 2>& edges);
 
 // Spatial-hashing detection algorithm
 ImpactsPtr detect_edge_vertex_collisions_hash_map(
-    const Eigen::MatrixXd& vertices_t0,
-    const Eigen::MatrixXd& vertices_t1,
+    const Eigen::MatrixXd& vertices_t0, const Eigen::MatrixXd& vertices_t1,
     const Eigen::Matrix<int, Eigen::Dynamic, 2>& edges);
 
-}
+} // namespace ccd
 #endif
--- src/collision_detection.cpp
+++ src/collision_detection.cpp
@@ -6,22 +6,18 @@
 #include <iostream>
 
 namespace ccd {
-ImpactPtr detect_edge_vertex_collision(
-        const Eigen::MatrixXd& vertex0_t0,
-        const Eigen::MatrixXd& vertex0_t1,
-        const Eigen::MatrixXd& edge_vertex1_t0,
-        const Eigen::MatrixXd& edge_vertex1_t1,
-        const Eigen::MatrixXd& edge_vertex2_t0,
-        const Eigen::MatrixXd& edge_vertex2_t1){
+ImpactPtr detect_edge_vertex_collision(const Eigen::MatrixXd& vertex0_t0,
+    const Eigen::MatrixXd& vertex0_t1, const Eigen::MatrixXd& edge_vertex1_t0,
+    const Eigen::MatrixXd& edge_vertex1_t1,
+    const Eigen::MatrixXd& edge_vertex2_t0,
+    const Eigen::MatrixXd& edge_vertex2_t1)
+{
     throw "Not implemented yet";
 }
 
-
-ImpactsPtr detect_edge_vertex_collisions(
-    const Eigen::MatrixXd& vertices_t0,
+ImpactsPtr detect_edge_vertex_collisions(const Eigen::MatrixXd& vertices_t0,
     const Eigen::MatrixXd& vertices_t1,
-    const Eigen::Matrix<int, Eigen::Dynamic, 2>& edges,
-    DetectionMethod method)
+    const Eigen::Matrix<int, Eigen::Dynamic, 2>& edges, DetectionMethod method)
 {
     assert(vertices_t0.rows() == vertices_t1.rows());
     switch (method) {
@@ -35,18 +31,17 @@ ImpactsPtr detect_edge_vertex_collisions(
 }
 
 ImpactsPtr detect_edge_vertex_collisions_brute_force(
-    const Eigen::MatrixXd& vertices_t0,
-    const Eigen::MatrixXd& vertices_t1,
+    const Eigen::MatrixXd& vertices_t0, const Eigen::MatrixXd& vertices_t1,
     const Eigen::Matrix<int, Eigen::Dynamic, 2>& edges)
 {
     int dimensions = int(vertices_t0.cols());
     ImpactsPtr impacts(new Impacts());
     ImpactPtr potential_impact(nullptr);
-    for(int edge_index = 0; edge_index < edges.rows(); edge_index++){
+    for (int edge_index = 0; edge_index < edges.rows(); edge_index++) {
         auto edge = edges.middleRows<1>(edge_index);
-        for(int vertex_index = 0; vertex_index < vertices_t0.rows();
-                vertex_index++){
-            if(vertex_index != edge(0) && vertex_index != edge(1)){
+        for (int vertex_index = 0; vertex_index < vertices_t0.rows();
+             vertex_index++) {
+            if (vertex_index != edge(0) && vertex_index != edge(1)) {
                 potential_impact = detect_edge_vertex_collision(
                     vertices_t0.middleRows(vertex_index, 1),
                     vertices_t1.middleRows(vertex_index, 1),
@@ -54,7 +49,7 @@ ImpactsPtr detect_edge_vertex_collisions_brute_force(
                     vertices_t1.middleRows(edge(0), 1),
                     vertices_t0.middleRows(edge(1), 1),
                     vertices_t1.middleRows(edge(1), 1));
-                if(potential_impact){
+                if (potential_impact) {
                     impacts->push_back(potential_impact);
                 }
             }
@@ -71,16 +66,13 @@ ImpactsPtr detect_edge_vertex_collisions_hash_map(
     throw "Hash Map collision detection is not implemented yet.";
 }
 
-void test(){
+void test()
+{
     Eigen::Matrix<double, 3, 2, Eigen::RowMajor> vertices_t0;
     Eigen::Matrix<double, 3, 2, Eigen::RowMajor> vertices_t1;
     Eigen::Matrix<int, 1, 2, Eigen::RowMajor> edges;
-    vertices_t0 << 0, 2,
-                   1, 1,
-                   1, 3;
-    vertices_t1 << 1, 2,
-                   0, 1,
-                   0, 3;
+    vertices_t0 << 0, 2, 1, 1, 1, 3;
+    vertices_t1 << 1, 2, 0, 1, 0, 3;
     edges << 1, 2;
 
     detect_edge_vertex_collisions(vertices_t0, vertices_t1, edges);
