cc45e2b2dcee30065680c3f5d2b952943da2fb7a
Teseo Schneider <teseo@bluewin.ch>
2019-08-30 17:44:42 -0400

    new script to export simulations to eps

---
+++ tools/results_to_eps.py
@@ -0,0 +1,60 @@
+import argparse
+import json
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+
+import numpy as np
+
+
+def main(args=[]):
+    parser = argparse.ArgumentParser(
+        description='Make one eps file with the sequence')
+    parser.add_argument('results_file', metavar='input.json',
+                        type=Path,  help='result file to process')
+    parser.add_argument('--output_file', type=Path,
+                        default='output.eps', help='name of the output eps file')
+    parser.add_argument('--scaling', type=float,
+                        default=1.0, help='Scaling factor')
+    parser.add_argument('--colormap',
+                        default='Spectral', help='Colormap')
+    args = parser.parse_args()
+
+    fin = args.results_file
+    fout = args.output_file
+    cmap = plt.cm.get_cmap(args.colormap)
+    print(f"Saving to: {fout}")
+
+    with fin.open("r") as json_file:
+        results = json.load(json_file)["animation"]
+        vertices_sequence = results["vertices_sequence"]
+        edges = np.array(results["edges"], dtype=np.int32)
+
+    with fout.open("w") as eps_file:
+        bbox = None
+        for vs in vertices_sequence:
+            V = np.array(vs)*args.scaling
+            cbox = np.min(V[:, 0]), np.max(V[:, 0]), np.min(V[:, 1]), np.max(V[:, 1])
+            if bbox is None:
+                bbox = cbox
+            else:
+                bbox = min(bbox[0], cbox[0]), max(bbox[1], cbox[1]), min(bbox[2], cbox[2]), max(bbox[3], cbox[3])
+
+        eps_file.write("%!PS-Adobe-3.0 EPSF-3.0\n")
+        eps_file.write("%%BoundingBox: {} {} {} {}\n\n".format(bbox[0], bbox[2], bbox[1], bbox[3]))
+        eps_file.write("%%Pages: 1\n")
+        eps_file.write("%%Page: 1 1\n")
+        eps_file.write("/show-ctr {\ndup stringwidth pop\n -2 div 0\n rmoveto show\n} def\n\n 2 setlinejoin\n\n")
+
+        for i, vs in enumerate(vertices_sequence):
+            V = np.array(vs)*args.scaling
+            t = float(i)/(len(vertices_sequence)-1)
+            rgba = cmap(t)
+            eps_file.write("{} {} {} setrgbcolor\n".format(rgba[0], rgba[1], rgba[2]))
+            for e in edges:
+                eps_file.write("{} {} moveto\n".format(V[e[0], 0], V[e[0], 1]))
+                eps_file.write("{} {} lineto\n".format(V[e[1], 0], V[e[1], 1]))
+            eps_file.write("stroke\n\n")
+
+if __name__ == "__main__":
+    main()
