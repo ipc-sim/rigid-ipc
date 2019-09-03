8f8dd94c4f0bbd51fc93a25c8f192ff84a11b87f
pancha <fgu204@nyu.edu>
2019-09-03 10:12:26 -0400

    added fixture-script to test different eb values.

---
+++ tools/generate_falling_diamond_fixture.py
@@ -0,0 +1,30 @@
+import os
+import json
+import sys
+
+import argparse
+from pathlib import Path
+
+
+fixtures = Path(os.path.dirname(os.path.realpath(__file__))).joinpath("..","fixtures")
+base_scene = fixtures.joinpath("rigid_bodies_cor","barrier__gravity_falling_diamond_vert.json")
+
+def main(args=[]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output_file', metavar='output.json', type=Path,  help='result file to process')
+    parser.add_argument('--e_b', type=float)
+    args = parser.parse_args()
+
+    e_b = args.e_b
+    fout = Path(args.output_file)
+
+    with base_scene.open("r") as json_file:
+        data = json.load(json_file)
+
+    data["barrier_solver"]["e_b"] = e_b
+
+    with fout.open("w") as json_file:
+        json.dump(data, json_file, indent=4, separators=(',', ':'))
+
+if __name__ == "__main__":
+    main()
