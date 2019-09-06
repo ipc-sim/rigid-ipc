a4b26a450c500c9030def8d8f204356ce09b324c
Zachary Ferguson <zfergus@nyu.edu>
2019-09-06 19:23:08 -0400

    Improved check_results.sh

--- tools/check_results.sh
+++ tools/check_results.sh
@@ -7,16 +7,24 @@ LOGS=$(find $1 -name "*.out" -type f | grep ours)
 
 for f in $LOGS
 do
+    finished=$(cat "$f" | grep postprocess)
+    if [ -z "$finished" ]; then
+        continue
+    fi
     echo "$f:"
     errors=$(cat $f | grep -e "error" | grep -v -e "collisions_unsolved")
-    warnings=$(cat $f | grep -e "warning")
+    warnings=$(cat $f | grep -e "warning" | grep -v -e "collisions_unsolved")
 	if [ -n "$errors" ]; then
         # echo -e "\033[0;31mFailing\033[0m: $(echo "$errors" | head -1)"
-        echo -e "\033[0;31mErrors\033[0m: $errors"
+        first_error=$(echo "$errors" | head -n 1)
+        num_errors=$(echo "$errors" | wc -l)
+        printf "\033[0;31mErrors\033[0m: %s\n+%d more\n" "$first_error" "$num_errors"
     elif [ -n "$warnings" ]; then
-        echo -e "\033[1;33mWarnings\033[0m: $warnings"
+        first_warning=$(echo "$warnings" | head -n 1)
+        num_warnings=$(echo "$warnings" | wc -l)
+        printf "\033[1;33mWarnings\033[0m: %s\n+%d more\n" "$first_warning" "$num_warnings"
     else
-        echo -e "\033[0;32mPassing\033[0m"
+        printf "\033[0;32mPassing\033[0m\n"
     fi
     echo
 done
