35434740a674ccb9729b87f21b21e1784a98a188
zfergus <zfergus@nyu.edu>
2020-11-02 14:37:52 -0500

    Improved PL CCD by using Interval(0, 1) for ti

--- src/ccd/piecewise_linear/time_of_impact.cpp
+++ src/ccd/piecewise_linear/time_of_impact.cpp
@@ -159,43 +159,47 @@ bool compute_piecewise_linear_edge_edge_time_of_impact(
 
         double min_distance = 0;
 #ifndef USE_FIXED_PIECES
-        Interval t(ti0, ti1);
+        Interval ti(0, 1);
 
-        PoseI poseIA_t0 = poseA_t0.cast<Interval>();
-        PoseI poseIA_t1 = poseA_t1.cast<Interval>();
-        PoseI poseIB_t0 = poseB_t0.cast<Interval>();
-        PoseI poseIB_t1 = poseB_t1.cast<Interval>();
+        PoseI poseIA_ti0 = poseA_ti0.cast<Interval>();
+        PoseI poseIA_ti1 = poseA_ti1.cast<Interval>();
+        PoseI poseIB_ti0 = poseB_ti0.cast<Interval>();
+        PoseI poseIB_ti1 = poseB_ti1.cast<Interval>();
 
-        PoseI poseIA = PoseI::interpolate(poseIA_t0, poseIA_t1, t);
-        PoseI poseIB = PoseI::interpolate(poseIB_t0, poseIB_t1, t);
+        PoseI poseIA = PoseI::interpolate(poseIA_ti0, poseIA_ti1, ti);
+        PoseI poseIB = PoseI::interpolate(poseIB_ti0, poseIB_ti1, ti);
 
         Eigen::MatrixXX3I RA = poseIA.construct_rotation_matrix();
         Eigen::MatrixXX3I RB = poseIB.construct_rotation_matrix();
 
         using Vector3I = Eigen::Vector3I;
 
-        Vector3I ea0_t0 = bodyA.world_vertex(poseA_t0, ea0i).cast<Interval>();
-        Vector3I ea0_t1 = bodyA.world_vertex(poseA_t1, ea0i).cast<Interval>();
+        Vector3I ea0_ti0 = bodyA.world_vertex(poseA_ti0, ea0i).cast<Interval>();
+        Vector3I ea0_ti1 = bodyA.world_vertex(poseA_ti1, ea0i).cast<Interval>();
         Vector3I ea0 = bodyA.world_vertex(RA, poseIA.position, ea0i);
-        Interval d = (ea0 - ((ea0_t1 - ea0_t0) * t + ea0_t0)).norm();
+        Interval d = (ea0 - ((ea0_ti1 - ea0_ti0) * ti + ea0_ti0)).norm();
+        assert(abs(d.lower()) < 1e-12); // The endpoints are part of both curves
         min_distance = std::max(min_distance, d.upper());
 
-        Vector3I ea1_t0 = bodyA.world_vertex(poseA_t0, ea1i).cast<Interval>();
-        Vector3I ea1_t1 = bodyA.world_vertex(poseA_t1, ea1i).cast<Interval>();
+        Vector3I ea1_ti0 = bodyA.world_vertex(poseA_ti0, ea1i).cast<Interval>();
+        Vector3I ea1_ti1 = bodyA.world_vertex(poseA_ti1, ea1i).cast<Interval>();
         Vector3I ea1 = bodyA.world_vertex(RA, poseIA.position, ea1i);
-        d = (ea1 - ((ea1_t1 - ea1_t0) * t + ea1_t0)).norm();
+        d = (ea1 - ((ea1_ti1 - ea1_ti0) * ti + ea1_ti0)).norm();
+        assert(abs(d.lower()) < 1e-12); // The endpoints are part of both curves
         min_distance = std::max(min_distance, d.upper());
 
-        Vector3I eb0_t0 = bodyB.world_vertex(poseB_t0, eb0i).cast<Interval>();
-        Vector3I eb0_t1 = bodyB.world_vertex(poseB_t1, eb0i).cast<Interval>();
+        Vector3I eb0_ti0 = bodyB.world_vertex(poseB_ti0, eb0i).cast<Interval>();
+        Vector3I eb0_ti1 = bodyB.world_vertex(poseB_ti1, eb0i).cast<Interval>();
         Vector3I eb0 = bodyB.world_vertex(RB, poseIB.position, eb0i);
-        d = (eb0 - ((eb0_t1 - eb0_t0) * t + eb0_t0)).norm();
+        d = (eb0 - ((eb0_ti1 - eb0_ti0) * ti + eb0_ti0)).norm();
+        assert(abs(d.lower()) < 1e-12); // The endpoints are part of both curves
         min_distance = std::max(min_distance, d.upper());
 
-        Vector3I eb1_t0 = bodyB.world_vertex(poseB_t0, eb1i).cast<Interval>();
-        Vector3I eb1_t1 = bodyB.world_vertex(poseB_t1, eb1i).cast<Interval>();
+        Vector3I eb1_ti0 = bodyB.world_vertex(poseB_ti0, eb1i).cast<Interval>();
+        Vector3I eb1_ti1 = bodyB.world_vertex(poseB_ti1, eb1i).cast<Interval>();
         Vector3I eb1 = bodyB.world_vertex(RB, poseIB.position, eb1i);
-        d = (eb1 - ((eb1_t1 - eb1_t0) * t + eb1_t0)).norm();
+        d = (eb1 - ((eb1_ti1 - eb1_ti0) * ti + eb1_ti0)).norm();
+        assert(abs(d.lower()) < 1e-12); // The endpoints are part of both curves
         min_distance = std::max(min_distance, d.upper());
 
         if (min_distance >= 0.5 * distance_ti0) {
@@ -323,43 +327,47 @@ bool compute_piecewise_linear_face_vertex_time_of_impact(
 
         double min_distance = 0;
 #ifndef USE_FIXED_PIECES
-        Interval t(ti0, ti1);
+        Interval ti(0, 1);
 
-        PoseI poseIA_t0 = poseA_t0.cast<Interval>();
-        PoseI poseIA_t1 = poseA_t1.cast<Interval>();
-        PoseI poseIB_t0 = poseB_t0.cast<Interval>();
-        PoseI poseIB_t1 = poseB_t1.cast<Interval>();
+        PoseI poseIA_ti0 = poseA_ti0.cast<Interval>();
+        PoseI poseIA_ti1 = poseA_ti1.cast<Interval>();
+        PoseI poseIB_ti0 = poseB_ti0.cast<Interval>();
+        PoseI poseIB_ti1 = poseB_ti1.cast<Interval>();
 
-        PoseI poseIA = PoseI::interpolate(poseIA_t0, poseIA_t1, t);
-        PoseI poseIB = PoseI::interpolate(poseIB_t0, poseIB_t1, t);
+        PoseI poseIA = PoseI::interpolate(poseIA_ti0, poseIA_ti1, ti);
+        PoseI poseIB = PoseI::interpolate(poseIB_ti0, poseIB_ti1, ti);
 
         Eigen::MatrixXX3I RA = poseIA.construct_rotation_matrix();
         Eigen::MatrixXX3I RB = poseIB.construct_rotation_matrix();
 
         using Vector3I = Eigen::Vector3I;
 
-        Vector3I v_t0 = bodyA.world_vertex(poseA_t0, vi).cast<Interval>();
-        Vector3I v_t1 = bodyA.world_vertex(poseA_t1, vi).cast<Interval>();
+        Vector3I v_ti0 = bodyA.world_vertex(poseA_ti0, vi).cast<Interval>();
+        Vector3I v_ti1 = bodyA.world_vertex(poseA_ti1, vi).cast<Interval>();
         Vector3I v = bodyA.world_vertex(RA, poseIA.position, vi);
-        Interval d = (v - ((v_t1 - v_t0) * t + v_t0)).norm();
+        Interval d = (v - ((v_ti1 - v_ti0) * ti + v_ti0)).norm();
+        assert(abs(d.lower()) < 1e-12); // The endpoints are part of both curves
         min_distance = std::max(min_distance, d.upper());
 
-        Vector3I f0_t0 = bodyB.world_vertex(poseB_t0, f0i).cast<Interval>();
-        Vector3I f0_t1 = bodyB.world_vertex(poseB_t1, f0i).cast<Interval>();
+        Vector3I f0_ti0 = bodyB.world_vertex(poseB_ti0, f0i).cast<Interval>();
+        Vector3I f0_ti1 = bodyB.world_vertex(poseB_ti1, f0i).cast<Interval>();
         Vector3I f0 = bodyB.world_vertex(RB, poseIB.position, f0i);
-        d = (f0 - ((f0_t1 - f0_t0) * t + f0_t0)).norm();
+        d = (f0 - ((f0_ti1 - f0_ti0) * ti + f0_ti0)).norm();
+        assert(abs(d.lower()) < 1e-12); // The endpoints are part of both curves
         min_distance = std::max(min_distance, d.upper());
 
-        Vector3I f1_t0 = bodyB.world_vertex(poseB_t0, f1i).cast<Interval>();
-        Vector3I f1_t1 = bodyB.world_vertex(poseB_t1, f1i).cast<Interval>();
+        Vector3I f1_ti0 = bodyB.world_vertex(poseB_ti0, f1i).cast<Interval>();
+        Vector3I f1_ti1 = bodyB.world_vertex(poseB_ti1, f1i).cast<Interval>();
         Vector3I f1 = bodyB.world_vertex(RB, poseIB.position, f1i);
-        d = (f1 - ((f1_t1 - f1_t0) * t + f1_t0)).norm();
+        d = (f1 - ((f1_ti1 - f1_ti0) * ti + f1_ti0)).norm();
+        assert(abs(d.lower()) < 1e-12); // The endpoints are part of both curves
         min_distance = std::max(min_distance, d.upper());
 
-        Vector3I f2_t0 = bodyB.world_vertex(poseB_t0, f2i).cast<Interval>();
-        Vector3I f2_t1 = bodyB.world_vertex(poseB_t1, f2i).cast<Interval>();
+        Vector3I f2_ti0 = bodyB.world_vertex(poseB_ti0, f2i).cast<Interval>();
+        Vector3I f2_ti1 = bodyB.world_vertex(poseB_ti1, f2i).cast<Interval>();
         Vector3I f2 = bodyB.world_vertex(RB, poseIB.position, f2i);
-        d = (f2 - ((f2_t1 - f2_t0) * t + f2_t0)).norm();
+        d = (f2 - ((f2_ti1 - f2_ti0) * ti + f2_ti0)).norm();
+        assert(abs(d.lower()) < 1e-12); // The endpoints are part of both curves
         min_distance = std::max(min_distance, d.upper());
 
         if (min_distance >= 0.5 * distance_ti0) {
