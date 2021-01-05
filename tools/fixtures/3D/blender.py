9a62805d319f26d670bf33c20397892c34f9e03e
Zachary Ferguson <zfergus@nyu.edu>
2021-01-05 12:15:08 -0500

    Updated fixtures to remove large dhat and DMV time-stepper
    * Adjusted kinematic scenes for next commit

--- tools/fixtures/3D/blender.py
+++ tools/fixtures/3D/blender.py
@@ -11,23 +11,19 @@ import fixture_utils
 def main():
     random.seed(0)
 
-    # num_monkeys = 1
-    # if(len(sys.argv) > 1):
-    #     num_monkeys = int(sys.argv[1])
+    num_monkeys = 10
+    if(len(sys.argv) > 1):
+        num_monkeys = int(sys.argv[1])
 
     scene = {
         "scene_type": "distance_barrier_rb_problem",
         "solver": "ipc_solver",
         "timestep": 0.005,
         "max_time": 5,
-        "distance_barrier_constraint": {
-            "initial_barrier_activation_distance": 1e-2
-        },
         "rigid_body_problem": {
             "coefficient_restitution": -1,
             "coefficient_friction": 0,
             "gravity": [0, -9.8, 0],
-            "time_stepper": "dmv",
             "rigid_bodies": [{
                 "mesh": "plane.obj",
                 "position": [0, 0, 0],
@@ -37,13 +33,12 @@ def main():
                 "angular_velocity": [0, 200, 0],
                 "torque": [0, 0, 0],
                 "is_dof_fixed": [True, True, True, True, False, True],
-                "group_id": 0
+                "type": "kinematic"
             }, {
                 "mesh": "pot.obj",
                 "position": [0, 0.61, 0],
                 "scale": [2.2, 1, 2.2],
-                "is_dof_fixed": True,
-                "group_id": 0
+                "type": "static"
             }]
         }
     }
@@ -57,10 +52,13 @@ def main():
     rbs = scene["rigid_body_problem"]["rigid_bodies"]
     # rbs.append(monkey)
 
-    num_x = 5
-    num_y = 8
-    num_z = 5
-    num_monkeys = num_x * num_y * num_z
+    if num_monkeys == 10:
+        num_x, num_y, num_z = 5, 1, 2
+    elif num_monkeys % 25 == 0:
+        num_x, num_y, num_z = 5, num_monkeys // 25, 5
+    else:
+        num_x, num_y, num_z = 1, num_monkeys, 1
+    assert(num_monkeys == num_x * num_y * num_z)
 
     for x in range(num_x):
         for y in range(num_y):
