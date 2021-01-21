60c09e470dbe0de01f3cf44a4707cbcd875d8d2a
Zachary Ferguson <zfergus@nyu.edu>
2021-01-21 15:51:01 -0500

    Updated mechanism fixtures

--- tools/benchmarks/mechanisms.py
+++ tools/benchmarks/mechanisms.py
@@ -5,7 +5,7 @@ print_fixtures("mechanisms", [
     "mechanisms/507-movements/140-punching-machine",
     # "mechanisms/507-movements/227-chain-pully",
     "mechanisms/507-movements/398-intermitten-motion",
-    "mechanisms/507-movements/493-Lewis.json",
+    # "mechanisms/507-movements/493-Lewis.json",
     "mechanisms/expanding-lock-box",
     "mechanisms/screw",
     "mechanisms/two-way-screw",
