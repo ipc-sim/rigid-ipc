5f240817fa5427a3503c50bd2c42c9296ea0df16
Zachary Ferguson <zfergus@nyu.edu>
2021-01-24 19:10:43 -0500

    Updated bike-chain

---
+++ meshes/507-movements/227-chain-pully/sprocket-20teeth.scad
@@ -0,0 +1,166 @@
+/*
+
+Sprocket generator v2
+
+This code is based on the code written by *Talon_1* who based his code on the work of
+*Aleksejs*. Big thanks for your contributions. The aim of this code is to be easier
+understood by folks that are new to OpenSCAD. The rendered sprocket can be downloaded 
+as a .STL file and 3D-printed directly using any slicing program.
+
+*/
+
+//////////////////////
+/* CHAIN-PARAMETERS */
+//////////////////////
+
+// THESE ARE FOR 25H/04C
+roller_d  = 4;
+thickness = 1.66;
+pitch     = 7;
+tolerance = 0.05;
+
+///////////////
+/* VARIABLES */
+///////////////
+
+teeth    = 20;
+
+// Shaft
+bottom_shaft_d = 20;
+bottom_shaft_h = 0; // = 0 to remove
+
+top_shaft_d    = 40;
+top_shaft_h    = 0; // = 0 to remove
+
+toptop_shaft_d = 36;
+toptop_shaft_h = 0; // = 0 to remove
+
+// Bore
+hole_d   = 15;
+
+// Holes
+number_of_holes = -1;
+hole_dia = 4;
+hole_ring_dia = 30;
+
+///////////////////////
+// RENDERING QUALITY */
+///////////////////////
+
+// HIGH : fs=0.25 : fa=3 : fn=0
+// LOW  : fs=1    : fa=7 : fn=0
+fs = 0.25; // Minimum size of a fragment
+fa = 3; // Minimum angle for a fragment
+fn = 0; // Number of fragments (overrides fs & fa if non zero)
+
+///////////////
+/* MAIN CODE */
+///////////////
+
+difference()
+{
+    // Create a union of four shapes, 3 cylinders and 1 sprocket
+    union()
+    {
+        // Create sprocket using the difined module
+        sprocket(teeth, roller_d, pitch, thickness, tolerance);
+        
+        // Create cylinder on front side of sprocket
+        translate([0, 0, thickness])
+            cylinder(top_shaft_h, top_shaft_d/2, top_shaft_d/2, $fs=fs, $fa=fa, $fn=fn);
+        
+        // Create cylinder on back side of sprocket
+        rotate([0,180])
+            cylinder(bottom_shaft_h, bottom_shaft_d/2, bottom_shaft_d/2, $fs=fs, $fa=fa, $fn=fn);
+        
+        // Create cylinder on top of the front side cylinder
+        translate([0, 0, thickness+top_shaft_h])
+            cylinder(toptop_shaft_h, toptop_shaft_d/2, toptop_shaft_d/2, $fs=fs, $fa=fa, $fn=fn);
+    }
+    
+    // Rest of shapes are removal of material
+    // Drills out the center hole with 1 mm extra in both directions
+    translate([0, 0, -bottom_shaft_h-1])
+    {        
+        cylinder(bottom_shaft_h+thickness+top_shaft_h+toptop_shaft_h+2, hole_d/2, hole_d/2, $fs=fs, $fa=fa, $fn=fn);
+    }
+
+    // Drills 'number_of_holes' many holes in a circle
+    angle_between_holes = 360/number_of_holes;
+    for(hole_angle = [0:360/number_of_holes:360])
+    {
+        translate([hole_ring_dia/2*cos(hole_angle), hole_ring_dia/2*sin(hole_angle), -bottom_shaft_h-1])
+        {
+            cylinder(h = bottom_shaft_h+thickness+top_shaft_h+toptop_shaft_h+2, r = hole_dia/2, $fs=fs, $fa=fa, $fn=fn);
+        }
+    }
+}
+
+/////////////////////
+/* SPROCKET MODULE */
+/////////////////////
+
+module sprocket(teeth=20, roller=3, pitch=17, thickness=3, tolerance=0.2)
+{
+	roller_radius = roller/2; //We need radius in our calculations, not diameter
+	distance_from_center = pitch/(2*sin(180/teeth));
+	angle = (360/teeth);
+	
+    pitch_radius = sqrt((distance_from_center*distance_from_center) - (pitch*(roller_radius+tolerance))+((roller_radius+tolerance)*(roller_radius+tolerance)));
+	    
+    difference()
+    {
+		union()
+        {
+            // Quality parameters
+            $fs = fs; 
+            $fa = fa;
+            $fn = fn;
+            
+            // Create inner cylinder with radius = pitch_radius
+			cylinder(r=pitch_radius, h=thickness);
+            
+            // Create outer part of the teeth
+			for(tooth=[1:teeth])
+            {
+				intersection()
+                {
+					rotate(a=[0, 0, angle*(tooth+0.5)])
+                    {
+						translate([distance_from_center, 0, 0])
+                        {
+                            $fs = fs; 
+                            $fa = fa;
+                            $fn = fn;
+							cylinder(r=pitch-roller_radius-tolerance, h=thickness);
+						}
+					}
+					rotate(a=[0,0,angle*(tooth-0.5)])
+                    {
+						translate([distance_from_center,0,0])
+                        {
+							cylinder(r=pitch-roller_radius-tolerance,h=thickness);
+						}
+					}
+				}
+			}
+		}
+        
+        // Cuts away the inner groove between the teeth
+		for(tooth=[1:teeth])
+        {
+			rotate(a=[0, 0, angle*(tooth+0.5)])
+            {
+				translate([distance_from_center, 0, -1])
+                {
+					$fs = fs; 
+                    $fa = fa;
+                    $fn = fn;
+                    cylinder(r=roller_radius+tolerance, h=thickness+2);
+				}
+			}
+		}
+	}
+}
+
+
--- tools/fixtures/3D/chain_pully.py
+++ tools/fixtures/3D/chain_pully.py
@@ -1,4 +1,3 @@
-from scipy.spatial.transform import Rotation as R
 import pymesh
 import json
 import pathlib
@@ -7,12 +6,13 @@ import math
 import itertools
 
 import numpy
+from scipy.spatial.transform import Rotation
 
 import context
 
-from fixture_utils import save_fixture, get_fixture_dir_path
+from fixture_utils import save_fixture, get_fixture_dir_path, get_meshes_dir_path
 
-scale = 1
+scale = 0.01
 
 barring = {
     "mesh": "507-movements/227-chain-pully/barring.obj",
@@ -36,23 +36,94 @@ link_width = 2 * link_hole_center
 link_vertical_offsets = [0.763387, 0.940965]
 
 
-def circle_point(angle_offset=0):
-    angles = numpy.linspace(angle_offset, 2 * numpy.pi + angle_offset, num=num_links,
-                            endpoint=False).reshape(-1, 1)
-    radius = link_width / (2 * numpy.sin(numpy.pi / num_links))
+def circle_points(num_links, angle_offset=0, radius_offset=0):
+    angles = numpy.linspace(angle_offset, 2 * numpy.pi + angle_offset,
+                            num=num_links, endpoint=False).reshape(-1, 1)
+    radius = link_width / (2 * numpy.sin(numpy.pi / num_links)) + radius_offset
     x = radius * numpy.cos(angles)
     y = radius * numpy.sin(angles)
     z = numpy.zeros(angles.shape)
-    points = numpy.hstack([x, y, z])
+    return numpy.hstack([x, y, z])
+
+
+def line_points(start, dir, num_links):
+    points = numpy.empty((num_links + 1, 3))
+    points[0] = start
+    dir /= numpy.linalg.norm(dir)
+    for i in range(num_links):
+        points[i + 1] = points[i] + link_width * dir
+    return points
+
+
+def export_polyline(points, offset=0, loops=True):
+    for point in points:
+        print("v {:g} {:g} {:g}".format(*point))
+    for i in range(len(points) - 1):
+        print(f"l {i + 1 + offset:d} {i + 2 + offset:d}")
+    if loops:
+        print(f"l {len(points) + offset:d} {1 + offset:d}")
 
 
 def polyline_to_chain(points):
     """Assumes the lines are the links length"""
+    assert((points[0] != points[-1]).any())  # no loop
     chain = []
-    for point in points:
-        chain.append(copy.deepcopy(barring))
-        chain.append(copy.deepcopy(pin))
+    num_points = points.shape[0]
+    assert(num_points % 2 == 0)
+    for i in range(num_points):
+        pi0 = points[i]
+        pi1 = points[(i + 1) % num_points]
+
         chain.append(copy.deepcopy(link))
+        chain[-1]["position"] = (scale * (pi1 + pi0) / 2).tolist()
+        chain[-1]["position"][2] = scale * link_vertical_offsets[i % 2]
+        chain[-1]["rotation"][2] = numpy.arctan2(
+            *(pi1 - pi0)[:2][::-1]) * 180 / numpy.pi
+
+        chain.append(copy.deepcopy(chain[-1]))
+        chain[-1]["position"][2] *= -1
+
+        chain.append(copy.deepcopy(barring))
+        chain[-1]["position"] = (scale * pi0).tolist()
+        chain.append(copy.deepcopy(pin))
+        chain[-1]["position"] = (scale * pi1).tolist()
+    return chain
+
+
+def generate_sprocket(num_links):
+    angles = numpy.linspace(0, 2 * numpy.pi, num=num_links,
+                            endpoint=False).reshape(-1, 1)
+    angle_offset = (angles[0] + angles[1]) / 2
+    points = circle_points(num_links, angle_offset, radius_offset=-1.5)
+
+    spike = pymesh.load_mesh(str(get_meshes_dir_path() /
+                                 "507-movements/227-chain-pully/spike.obj"))
+    spikes = []
+    for i in range(num_links):
+        R = Rotation.from_euler(
+            'xyz', [90, 0, (i + 0.5) * 360 / num_links], degrees=True)
+        R = R.as_matrix()
+        spikes.append(
+            pymesh.form_mesh(spike.vertices @ R.T + points[i], spike.faces)
+        )
+
+    points = circle_points(num_links, 0, radius_offset=-1)
+    x = points[:, 0].reshape(-1, 1)
+    y = points[:, 1].reshape(-1, 1)
+    points = numpy.vstack([numpy.hstack([x, y, numpy.full(angles.shape, spike.vertices[:, 1].min())]),
+                           numpy.hstack([x, y, numpy.full(angles.shape, spike.vertices[:, 1].max())])])
+
+    sprocket = pymesh.convex_hull(
+        pymesh.form_mesh(points, numpy.empty((0, 3))))
+
+    print("Union of spikes")
+    for spike in spikes:
+        sprocket = pymesh.boolean(sprocket, spike, operation="union")
+    print("Done")
+
+    pymesh.save_mesh(str(get_meshes_dir_path() /
+                         f"507-movements/227-chain-pully/sprocket-{num_links}teeth.obj"),
+                     sprocket)
 
 
 def main():
@@ -70,75 +141,58 @@ def main():
         }
     }
 
-    num_links = 20
+    num_links_1 = 20
+    num_links_2 = 8
 
-    angles = numpy.linspace(0, 2 * numpy.pi, num=num_links,
-                            endpoint=False).reshape(-1, 1)
-    radius = link_width / (2 * numpy.sin(numpy.pi / num_links))
-    x = radius * numpy.cos(angles)
-    y = radius * numpy.sin(angles)
-    z = numpy.zeros(angles.shape)
-    points = numpy.hstack([x, y, z])
+    cpoints1 = circle_points(num_links_1)
+    cpoints1 = cpoints1[:cpoints1.shape[0] // 2 + 1]
+    cpoints2 = circle_points(num_links_2)
+    cpoints2 = cpoints2[:cpoints2.shape[0] // 2 + 1]
+    cpoints2[:, 1] *= -1
 
+    dx = cpoints2[-1, 0] - cpoints1[-1, 0]
+    dlen = 10 * link_width
+    dy = numpy.sqrt(dlen**2 - dx**2)
+    cpoints2[:, 1] -= dy
+
+    dir1 = numpy.array([dx, -dy, 0])
+    lpoints1 = line_points(cpoints1[-1], dir1, 10)
+    dir2 = dir1.copy()
+    dir2[1] *= -1
+    lpoints2 = line_points(cpoints2[0], dir2, 10)
+
+    points = numpy.vstack(
+        [cpoints1[:],  # circle
+         lpoints1[1:-1],  # line down
+         cpoints2[::-1],
+         lpoints2[1:-1]  # line up
+         ])
+
+    R = numpy.array([[0, 1, 0],
+                     [-1, 0, 0],
+                     [0, 0, 1]])
+    points = points @ R.T
+
+    # export_polyline(points, offset=0, loops=True)
+
+    scene["rigid_body_problem"]["rigid_bodies"] = polyline_to_chain(points)
     bodies = scene["rigid_body_problem"]["rigid_bodies"]
 
-    for i in range(num_links):
-        bodies.append(copy.deepcopy(link))
-        bodies[-1]["position"] = (
-            scale * (points[(i + 1) % num_links] + points[i]) / 2).tolist()
-        bodies[-1]["position"][2] = scale * link_vertical_offsets[i % 2]
-        bodies[-1]["rotation"][2] = numpy.arctan2(
-            *(points[(i + 1) % num_links] - points[i])[:2][::-1]) * 180 / numpy.pi
-        # bodies[-1]["enabled"] = i == 0
-        bodies.append(copy.deepcopy(bodies[-1]))
-        bodies[-1]["position"][2] *= -1
-        bodies.append(copy.deepcopy(barring))
-        bodies[-1]["position"] = (scale * points[i]).tolist()
-        bodies.append(copy.deepcopy(pin))
-        bodies[-1]["position"] = (scale * points[i]).tolist()
-
-    # angle0 = (angles[0] + angles[1]) / 2
-    # angles = numpy.linspace(
-    #     angle0, angle0 + 2 * numpy.pi, num=num_links, endpoint=False).reshape(-1, 1)
-    # radius = link_width / (2 * numpy.sin(numpy.pi / num_links)) - 1.5
-    # x = radius * numpy.cos(angles)
-    # y = radius * numpy.sin(angles)
-    # z = numpy.zeros(angles.shape)
-    # points = numpy.hstack([x, y, z])
-    #
-    # spike = pymesh.load_mesh("/Users/zachary/Downloads/spike.obj")
-    # spikes = []
-    #
-    #
-    # for i in range(num_links):
-    #     spikes.append(
-    #         pymesh.form_mesh(
-    #             spike.vertices @ R.from_euler(
-    #                 'xyz', [90, 0, (i + 0.5) * 360 / num_links], degrees=True).as_matrix().T + points[i],
-    #             spike.faces)
-    #     )
-    #
-    #
-    # radius = link_width / (2 * numpy.sin(numpy.pi / num_links)) - 1.25
-    # x = radius * numpy.cos(angles)
-    # y = radius * numpy.sin(angles)
-    # points = numpy.vstack([
-    #     numpy.hstack(
-    #         [x, y, numpy.full(angles.shape, spike.vertices.min(axis=0)[1])]),
-    #     numpy.hstack([x, y, numpy.full(angles.shape, spike.vertices.max(axis=0)[1])])])
-    #
-    # gear = pymesh.convex_hull(pymesh.form_mesh(points, numpy.empty((0, 3))))
-    #
-    # for spike in spikes:
-    #     gear = pymesh.boolean(gear, spike, operation="union")
-    #
-    # pymesh.save_mesh("gear.obj", gear)
-
+    # generate_sprocket(num_links_1)
     bodies.append({
-        "mesh": "gear.obj",
+        "mesh": "507-movements/227-chain-pully/sprocket-20teeth.obj",
         "angular_velocity": [0, 0, 100],
         "scale": scale,
-        "type": "kinematic"
+        "type": "kinematic",
+        "is_dof_fixed": ([True] * 5 + [False])
+    })
+
+    # generate_sprocket(num_links_2)
+    bodies.append({
+        "mesh": "507-movements/227-chain-pully/sprocket-8teeth.obj",
+        "scale": scale,
+        "type": "dynamic",
+        "is_dof_fixed": ([True] * 5 + [False])
     })
 
     save_fixture(scene, get_fixture_dir_path() / "3D" /
