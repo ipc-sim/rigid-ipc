379a9fc04ee92659bf2dffbaeac6b4320c2be6a1
Zachary Ferguson <zfergus@nyu.edu>
2021-09-07 21:49:56 -0400

    Moved get_rss.cpp -> get_rss.c

--- src/utils/get_rss.hpp
+++ src/utils/get_rss.hpp
@@ -9,10 +9,10 @@
  * Returns the peak (maximum so far) resident set size (physical memory use)
  * measured in bytes, or zero if the value cannot be determined on this OS.
  */
-size_t getPeakRSS();
+extern "C" size_t getPeakRSS();
 
 /**
  * Returns the current resident set size (physical memory use) measured in
  * bytes, or zero if the value cannot be determined on this OS.
  */
-size_t getCurrentRSS();
+extern "C" size_t getCurrentRSS();
