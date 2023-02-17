97c3815888085742aa6e63088f789b82aae17953
Zachary Ferguson <zfergus@nyu.edu>
2023-02-17 00:01:43 -0500

    Update libigl cmake

--- cmake/recipes/libigl.cmake
+++ cmake/recipes/libigl.cmake
@@ -15,6 +15,8 @@ endif()
 
 message(STATUS "Third-party: creating target 'igl::core'")
 
+set(LIBIGL_PREDICATES ON CACHE BOOL "Use exact predicates" FORCE)
+
 include(eigen)
 
 include(FetchContent)
